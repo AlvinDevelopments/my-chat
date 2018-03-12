9bf4f3b3d856b042956e02fe6fb93c63c0839be8
Alvin Li <alvinli@Alvins-MBP-3.hitronhub.home>
2018-03-11 23:24:44 -0600

    refined features

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
+
+var users = [];
+var connections = [];
+var messages = [];
 
 http.listen( port, function () {
     console.log('listening on port', port);
@@ -11,8 +15,124 @@ http.listen( port, function () {
 app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
-io.on('connection', function(socket){
+io.sockets.on('connection', function(socket){
+
+  io.emit('update users',users);
+  socket.emit('update messages', messages);
+
+
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  socket.on('add user',function(data){
+    let user = data;
+    socket.username = user;
+    console.log('User added', data);
+
+    if(checkUser(data)){
+      console.log('checked');
+      users.push(data);
+      console.log(users);
+      let m = generateMessage(user+' has connected.',users[users.indexOf(socket.username)],1,'black');
+      io.emit('chat', m);
+      messages.push(m);
+      console.log(messages);
+    }
+    else{
+      console.log('error, name exists');
+    }
+    io.emit('update users',users);
+
+  });
+
+
+  socket.on('request name',function(data){
+    let username='User'+Math.floor((Math.random() * 100) + 1).toString();
+    socket.username = username;
+    socket.emit('username',username);
+  });
+
+  socket.on('update user',function(data){
+    let user = data;
+    let oldname = socket.username;
+    console.log('User changed name to', data);
+    console.log(users[users.indexOf(socket.username)]);
+    if(checkUser(data)){
+      users[users.indexOf(socket.username)]=data;
+      socket.username = user;
+    }
+    else{
+      console.log('error, name exists');
+    }
+    // io.emit('notice',generateMessage('User '+socket.username+' changed name to '+data,users[users.indexOf(socket.username)],1));
+    io.emit('chat',generateMessage('User '+oldname+' changed name to '+data,users[users.indexOf(socket.username)],1,'black'));
+    console.log(users[users.indexOf(socket.username)]);
+    messages.push(generateMessage('User '+oldname+' changed name to '+data,users[users.indexOf(socket.username)],1,'black'));
+    io.emit('update users',users);
+  });
+
     socket.on('chat', function(msg){
-	io.emit('chat', msg);
+
+      let m = generateMessage(msg.msg,users[users.indexOf(socket.username)],0,msg.clr);
+      console.log('message: ' + msg.msg+' color is '+msg.clr);
+
+      io.emit('chat',m);
+      messages.push(m);
+      io.emit('update users',users);
     });
-});
+
+    socket.on('debug',function(data){
+      console.log(data);
+    });
+
+    // Disconnect
+    socket.on('disconnect', function(data){
+      connections.splice(connections.indexOf(socket),1);
+      console.log('Disconnected: %s sockets connected', connections.length);
+      let m = generateMessage(socket.username+' has disconnected.',users[users.indexOf(socket.username)],1,'black');
+      // io.emit('notice',m);
+      io.emit('chat',m);
+      messages.push(m);
+      users.splice(users.indexOf(socket.username), 1);
+      io.emit('update users',users);
+
+    });
+
+  });
+
+
+
+
+
+
+
+
+  function checkUser(name){
+    for(i=0;i<users.length;i++){
+      if(name==users[i]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+  function generateMessage(msg,user,type,color){
+    let date = new Date(Date.now());
+    // let day = date.getDate();
+    // let month = date.getMonth();
+    // let year = date.getFullYear();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    let m = {
+      user:user,
+      message:msg,
+      time:" ("+hour+":"+minute+":"+seconds+")",
+      color:color,
+      type: type
+    };
+
+    return m;
+  }
--- public/client.js
+++ public/client.js
@@ -1,12 +1,121 @@
 // shorthand for $(document).ready(...)
 $(function() {
-    var socket = io();
-    $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+
+    $('#userList').html('');
+
+    var username = getCookie('cname');
+    var socket = io.connect();
+    var color = 'black';
+
+    if(username!=''){
+      socket.emit('add user',username);
+    }
+    else{
+      socket.emit('request name',1);
+    }
+
+    socket.on('username', function(name){
+      username = name;
+      setCookie('cname',username,100);
+      socket.emit('add user',username);
     });
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+
+
+    $('form#message').submit(function(){
+      var m = $('#m').val();
+      if(m.startsWith('/nick ')){
+        username = m.substr(m.indexOf(" ") + 1);
+        setCookie('cname',username,100);
+        socket.emit('update user',username);
+         $('#m').val('');
+      }
+      else if(m.startsWith('/nickcolor ')){
+         color = m.substr(m.indexOf(" ") + 1);
+         $('#m').val('');
+      }
+      else{
+	     socket.emit('chat', {msg:$('#m').val(),clr:color});
+	      $('#m').val('');
+      }
+        $("#whole").scrollTop($("#whole")[0].scrollHeight);
+	      return false;
     });
-});
+
+    socket.on('chat', function(m){
+      if(m.user==username){
+
+        if(m.type==0){
+          $('#messages').append($('<li style="font-weight:bold; color:'+m.color+'">').text(m.time+" "+m.user+": "+m.message));
+        }
+        else{
+          $('#messages').append($('<li style="font-weight:bold; font-style:italic; color:'+m.color+'">').text(m.time+" "+m.message));
+        }
+      }
+      else{
+        if(m.type==0){
+	        $('#messages').append($(('<li style="color:'+m.color+'">')).text(m.time+" "+m.user+": "+m.message));
+        }
+        else{
+          $('#messages').append($('<li style="font-style:italic; color:'+m.color+'">').text(m.time+" "+m.user+" "+m.message));
+        }
+
+      }
+
+      // $("#messages").scrollTop($("#messages")[0].scrollHeight);
+      $("#whole").scrollTop($("#whole")[0].scrollHeight);
+
+    });
+
+    socket.on('update messages',function(msg){
+      let html = '';
+      for(i = 0; i < msg.length && i < 200; i++){
+        if(msg[i].type==0){
+          html+='<li style="color:'+msg[i].color+'">'+msg[i].time+" "+msg[i].user+": "+msg[i].message+'</l>';
+        }
+        else{
+          html+='<li style="font-style:italic; color:'+msg[i].color+'">'+msg[i].time+" "+msg[i].message+'</l>';
+        }
+      }
+      $('#messages').html(html);
+      $("#whole").scrollTop($("#whole")[0].scrollHeight);
+    });
+
+
+    socket.on('update users',function(users){
+      html='';
+      for(i = 0; i < users.length; i++){
+        if(users[i]==username){
+          html+='<li style="color: green">'+users[i]+'</l>';
+        }
+        else{
+          html+='<li>'+users[i]+'</l>';
+        }
+      }
+      $('#userList').html(html);
+    });
+
+  });
+
+// from w3
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+// from w3
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
